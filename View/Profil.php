833a34608eb000a431d0315b6171771f12296c21
Yanis <Sibachir>
2018-02-03 12:19:25 -0500

    Create Folders

---
+++ View/Profil.php
@@ -0,0 +1,14 @@
+<!DOCTYPE html>
+<html>
+
+<head>
+
+</head>
+
+<body>
+    <nav>
+
+    </nav>
+</body>
+
+</html>
---
+++ View/dashboard.php
@@ -0,0 +1,7 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: ahmed
+ * Date: 2018-02-03
+ * Time: 12:14 PM
+ */
